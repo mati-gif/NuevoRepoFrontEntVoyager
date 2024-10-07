7345df89524f819130afb32c6ee5ecec82080df9
mati-gif <matiassoria7700@gmail.com>
2024-10-07 13:24:03 -0300

    añadiendo modales en el home y cambiando los estilos del formulario de pagos con tarjeta

--- src/components/TimeInfoComponent.jsx
+++ src/components/TimeInfoComponent.jsx
@@ -64,6 +64,9 @@
 
 import React, { useEffect, useRef } from "react";
 import "../styles/timeInfo.css";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import { useSelector } from "react-redux";
 
 function TimeInfoComponent() {
     const containerRef = useRef(null);
@@ -83,6 +86,28 @@ function TimeInfoComponent() {
         return () => window.removeEventListener('scroll', handleScroll);
     }, []);
 
+    const navigate = useNavigate();
+    const status = useSelector(state => state.auth.status)
+    console.log(status);
+
+    const handleButtonClick = () => {
+        if (status === "success") {
+            navigate("/reservation");
+        } else {
+            Swal.fire({
+                icon: 'warning',
+                title: '',
+                text: 'To make a reservation you must first register or sign in',
+                // timer: 2000, // El temporizador dura 3 segundos (3000 milisegundos)
+                showConfirmButton: true, // Oculta el botón de confirmación
+                willClose: () => {
+                    navigate("/register"); // Navegar después de que la alerta desaparezca
+                }
+            });
+        }
+    };
+    
+
     return (
         <div className="happy-hours-container" ref={containerRef}>
             <div className="left-side-container-time-info">
@@ -92,7 +117,9 @@ function TimeInfoComponent() {
                         {/* El after office que combina lo mejor de la coctelería internacional
                         con la mística del rock */}
                     </p>
-                    <button className="text-[25px] text-yellow-400 border-2 border-yellow-500 p-[10px] hover:bg-[#0000002c] hover:text-yellow-400">
+                    <button 
+                    onClick={handleButtonClick}
+                    className="text-[25px] text-yellow-400 border-2 border-yellow-500 p-[10px] hover:bg-[#0000002c] hover:text-yellow-400">
                         <span>Make a reservation</span>
                     </button>
                 </div>
@@ -114,7 +141,8 @@ function TimeInfoComponent() {
                             <p>01:00</p>
                         </div>
                     </div>
-                    <button className="text-[25px] text-yellow-400 border-2 border-yellow-500 p-[10px] hover:bg-[#0000002c] hover:text-yellow-400">
+                    <button
+                    onClick={handleButtonClick} className="text-[25px] text-yellow-400 border-2 border-yellow-500 p-[10px] hover:bg-[#0000002c] hover:text-yellow-400">
                         <span>Make a reservation</span>
                     </button>
                 </div>
